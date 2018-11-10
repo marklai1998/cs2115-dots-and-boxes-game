e903a35235497213cb2b9a28c5f50f877710fddd
Mark Lai <marklai1998@gmail.com>
2018-11-10 20:00:33 +0800

    chore: update header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ import ghLogo from '../../assets/github-logo.svg'
 export const Header = () => (
   <header className='header'>
     <img src={logo} className='logo' alt='logo' />
-    <div className='title'>CS2115 Dots and Boxes Game</div>
+    <div className='title'>CS2115 Dots & Boxes Game</div>
     <a href='https://github.com/marklai1998/cs2115-dots-and-boxes-game'>
       <img src={ghLogo} className='ghLogo' alt='git-hub-logo' />
     </a>
