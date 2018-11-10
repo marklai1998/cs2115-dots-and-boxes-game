ec1df2139b8d999afa5d3d30a2627ff5ad6e6e18
Mark Lai <marklai1998@gmail.com>
2018-11-10 19:30:57 +0800

    chore:Initial dist subtree commit

---
+++ build/precache-manifest.52d80d99bc27ce75e6522224274d31a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7959c8ce33b8848e900316ae4e9b8f3f",
+    "url": "/cs2115-dots-and-boxes-game/static/media/logo.7959c8ce.svg"
+  },
+  {
+    "revision": "750dac63675afe3a04bd74fa0a8a1bee",
+    "url": "/cs2115-dots-and-boxes-game/static/media/github-logo.750dac63.svg"
+  },
+  {
+    "revision": "33389c4befa7cac5fe77",
+    "url": "/cs2115-dots-and-boxes-game/static/js/runtime~main.33389c4b.js"
+  },
+  {
+    "revision": "93aac2a6ad9751f5e0c1",
+    "url": "/cs2115-dots-and-boxes-game/static/js/main.93aac2a6.chunk.js"
+  },
+  {
+    "revision": "508198c3d744cc556579",
+    "url": "/cs2115-dots-and-boxes-game/static/js/1.508198c3.chunk.js"
+  },
+  {
+    "revision": "93aac2a6ad9751f5e0c1",
+    "url": "/cs2115-dots-and-boxes-game/static/css/main.a638f43e.chunk.css"
+  },
+  {
+    "revision": "0f095ddd18aab229f282f33d4d68a0c4",
+    "url": "/cs2115-dots-and-boxes-game/index.html"
+  }
+];
